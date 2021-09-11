cb6c0bdfaf524c634cdfd23cf5f8691d068a7964
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-11 00:27:58 -0300

    chore: add cors config

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log('allowing origin', origin);
+    callback(null, true);
+  },
+  credentials: true,
+};
 
 const authRoutes = require('./routes/auth.routes');
 const petRoutes = require('./routes/pet.routes');
 const userRoutes = require('./routes/user.routes');
 
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use('/auth', authRoutes);
 app.use('/pet', petRoutes);
 app.use('/user', userRoutes);
