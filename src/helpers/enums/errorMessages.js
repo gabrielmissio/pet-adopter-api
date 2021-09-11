925e67186fcabf5c97b5131213922c5b4b582c0d
gabrielmissio <gabmissil@gmail.com>
2021-09-11 11:52:09 -0300

    chore: add inactiev account message

--- src/helpers/enums/errorMessages.js
+++ src/helpers/enums/errorMessages.js
@@ -1,7 +1,8 @@
 const errorMessages = {
   USER_ALREADY_EXISTS: 'User already exists',
   USER_NOT_FOUND: 'User not found',
-  INVALID_CREDENTIALS: 'Invalid email or password'
+  INVALID_CREDENTIALS: 'Invalid email or password',
+  USER_WITH_INACTIVE_ACCOUNT: 'User with inactive account'
 };
 
 module.exports = Object.freeze(errorMessages);
