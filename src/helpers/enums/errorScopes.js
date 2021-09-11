f4ba83afc2f072f8a2d66c188fdf3be100e111b6
gabrielmissio <gabmissil@gmail.com>
2021-09-11 11:53:39 -0300

    chore: add inactive account scope

--- src/helpers/enums/errorScopes.js
+++ src/helpers/enums/errorScopes.js
@@ -1,7 +1,8 @@
 const errorScopes = {
   NOT_FOUND: 'NOT_FOUND',
   CONFLICT: 'CONFLICT',
-  INVALID_CREDENTIALS: 'INVALID_EMAIL_OR_PASSWORD'
+  INVALID_CREDENTIALS: 'INVALID_EMAIL_OR_PASSWORD',
+  INACTIVE_ACCOUNT: 'Inactive account'
 };
 
 module.exports = Object.freeze(errorScopes);
