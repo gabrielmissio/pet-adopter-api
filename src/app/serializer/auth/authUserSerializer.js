0abcaf46d4ee0cf9a090974e8af0fca69f221aad
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-11 19:50:43 -0300

    chore(auth): add accountStatus to authSerialize

--- src/app/serializer/auth/authUserSerializer.js
+++ src/app/serializer/auth/authUserSerializer.js
@@ -1,8 +1,9 @@
-const serialize = ({ id, name, email, createdAt }) => {
+const serialize = ({ id, name, email, createdAt, accountStatus }) => {
   return {
     id,
     name,
     email,
+    accountStatus,
     createdAt
   };
 };
