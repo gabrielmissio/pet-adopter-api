98a4cebd1807b82ae6430fa6385c5e0eeba8e1fa
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-12 00:01:07 -0300

    feat(user): add get user by email to UserRepository

--- src/app/repository/userRepository.js
+++ src/app/repository/userRepository.js
@@ -7,6 +7,24 @@ class UserRepository extends BaseRepository {
     super(User, USERS_TABLE_NAME); // call the super class constructor and pass in the name parameter
   }
 
+  async getUserByEmail(email) {
+    const { buildQueryParams } = require('./../mapper/clientMapper');
+    const { query } = require('./clientRepository');
+
+    const params = {
+      tableName: USERS_TABLE_NAME,
+      indexName: 'emailIndex',
+      keyConditionExpression: 'email = :email',
+      expressionAttributeValues: {
+        ':email': email
+      },
+      Limit: 1
+    };
+
+    const response = await query(buildQueryParams(params));
+    return response.Items[0];
+  }
+
 }
 
 module.exports = new UserRepository;
