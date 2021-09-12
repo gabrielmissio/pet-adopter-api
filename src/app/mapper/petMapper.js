50111f7f90d71fd583b8525a195fc7242e84e859
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-12 13:31:59 -0300

    refactor: remove unsused functions

--- src/app/mapper/petMapper.js
+++ src/app/mapper/petMapper.js
@@ -1,41 +1,5 @@
-const { v4: uuid } = require('uuid');
-const { PETS_TABLE_NAME } = require('./../../config');
-const { buildUpdatAccountStatusParams } = require('./commonMapper');
+// const { PETS_TABLE_NAME } = require('./../../config');
+ 
+// module.exports = {
 
-const buildCreatePetParams = payload => {
-  return {
-    tableName: PETS_TABLE_NAME,
-    item: payload
-  };
-};
-
-const buildGetPetsByStatusParams = status => {
-  return {
-    tableName: PETS_TABLE_NAME,
-    filterExpression: "accountStatus = :value",
-    expressionAttributeValues: {
-        ':value': status
-    }
-  };
-};
-
-const buildGetPetByIdParams = id => {
-  return {
-    tableName: PETS_TABLE_NAME,
-    key: {
-      'id': id
-    }
-  };
-};
-
-const buildUpdatePetAccountStatusParams = payload => {
-  payload.tableName = PETS_TABLE_NAME;
-  return buildUpdatAccountStatusParams(payload);
-};
-
-module.exports = {
-  buildCreatePetParams,
-  buildGetPetsByStatusParams,
-  buildGetPetByIdParams,
-  buildUpdatePetAccountStatusParams
-};
+// };
--- src/app/mapper/userMapper.js
+++ src/app/mapper/userMapper.js
@@ -1,5 +1,4 @@
 const { USERS_TABLE_NAME } = require('./../../config');
-const { buildUpdatAccountStatusParams } = require('./commonMapper');
 
 const buildGetUserByEmailParams = payload => {
   return {
@@ -13,53 +12,6 @@ const buildGetUserByEmailParams = payload => {
   };
 };
 
-const buildGetUserByIdParams = id => {
-  return {
-    tableName: USERS_TABLE_NAME,
-    key: {
-      'id': id
-    }
-  };
-};
-
-const buildUpdateUserParams = payload => {
-  return {
-    tableName: USERS_TABLE_NAME,
-    key: {
-      'id': payload.id
-    },
-    updateExpression: 'set :value',
-    expressionAttributeValues: {':value': payload.value}
-  };
-};
-
-const buildGetUsersByStatusParams = status => {
-  return {
-    tableName: USERS_TABLE_NAME,
-    filterExpression: "accountStatus = :value",
-    expressionAttributeValues: {
-        ':value': status
-    }
-  };
-};
-
-const buildCreateUserParams = payload => {
-  return {
-    tableName: USERS_TABLE_NAME,
-    item: payload
-  };
-};
-
-const buildUpdateUserAccountStatusParams = payload => {
-  payload.tableName = USERS_TABLE_NAME;
-  return buildUpdatAccountStatusParams(payload);
-};
-
 module.exports = {
-  buildGetUserByEmailParams,
-  buildGetUserByIdParams,
-  buildUpdateUserParams,
-  buildGetUsersByStatusParams,
-  buildCreateUserParams,
-  buildUpdateUserAccountStatusParams
+  buildGetUserByEmailParams
 };
