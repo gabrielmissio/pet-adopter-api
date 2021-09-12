43ae86598869b2c41884e36e75a633a1bab8b889
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-11 23:44:52 -0300

    feat: add create funtion to BaseRepository

--- src/app/repository/baseRepository.js
+++ src/app/repository/baseRepository.js
@@ -5,7 +5,7 @@ class BaseRepository {
     this.tableName = tableName;
   }
 
-  async getById (id) {
+  async getById(id) {
     const { buildGetParams } = require('./../mapper/clientMapper');
     const { get } = require('./clientRepository');
 
@@ -18,6 +18,19 @@ class BaseRepository {
 
     const response = await get(buildGetParams(params));
     return response.Item;
+  }
+
+  async create(payload) {
+    const { buildPutItemsParams } = require('./../mapper/clientMapper');
+    const { putItem } = require('./clientRepository');
+
+    const params = {
+      tableName: this.tableName,
+      item: payload
+    };
+
+    const response = await putItem(buildPutItemsParams(params));// TODO: validate response
+    return response;
   }
 };
 
