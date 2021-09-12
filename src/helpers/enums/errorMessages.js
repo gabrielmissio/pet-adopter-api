7f840d22eaabe5f5facc71822371a3a81024e38e
gabrielmissio <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-09-11 21:25:00 -0300

    feat(enums): add Pet not found error message

--- src/helpers/enums/errorMessages.js
+++ src/helpers/enums/errorMessages.js
@@ -2,7 +2,8 @@ const errorMessages = {
   USER_ALREADY_EXISTS: 'User already exists',
   USER_NOT_FOUND: 'User not found',
   INVALID_CREDENTIALS: 'Invalid email or password',
-  USER_WITH_INACTIVE_ACCOUNT: 'User with inactive account'
+  USER_WITH_INACTIVE_ACCOUNT: 'User with inactive account',
+  PET_NOT_FOUND: 'Pet not found'
 };
 
 module.exports = Object.freeze(errorMessages);
